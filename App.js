11e77067d62194cdac16cec731e3996f03f3396f
RezaIbnuH <rezaibnu752@gmail.com>
2024-12-03 09:11:17 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,45 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Nanda Alfiranda</Text>
+      <View style={styles.boxContainer}>
+        <View style={styles.box1} />
+        <View style={styles.box2} />
+      </View>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#d3d3d3', 
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 24,
+    color: 'blue',
+    fontWeight: 'bold',
+    marginBottom: 20, 
+  },
+  boxContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '80%',
+  },
+  box1: {
+    backgroundColor: 'red',
+    width: 100,
+    height: 100,
+  },
+  box2: {
+    backgroundColor: 'green',
+    width: 100,
+    height: 100,
+  },
+});
